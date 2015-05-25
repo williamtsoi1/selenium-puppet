bfc854b0fb864583159a2c8ed6cea9c5a880a487
William Tsoi <william@williamtsoi.net>
2015-05-25 21:51:40 +1000

    initial commit

---
+++ Puppetfile
@@ -0,0 +1,10 @@
+forge "https://forgeapi.puppetlabs.com"
+mod 'puppetlabs/java'
+mod 'puppetlabs/stdlib'
+mod 'puppetlabs/apt'
+mod 'p0deje/display'
+mod 'jhoblitt/selenium'
+mod 'p0deje/firefox'
+
+mod 'rip/module-data',
+  :git => 'https://github.com/ripienaar/puppet-module-data.git'
---
+++ manifests/default.pp
@@ -0,0 +1,14 @@
+node default {
+  include java
+  include display
+  include chrome-browser
+  class { 'selenium::hub': }
+  class { 'selenium::node': }
+  class { 'firefox':
+    version => '38.0.1-0ubuntu1'
+  }
+  Class['java'] -> Class['selenium::hub']
+  Class['java'] -> Class['selenium::node']
+  Class['display'] -> Class['selenium::node']
+  Class['firefox'] -> Class['selenium::node']
+}
