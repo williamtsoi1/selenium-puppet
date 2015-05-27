0febb70969e7df7d916e8d22ba7fb86f8b313d4e
William Tsoi <william@williamtsoi.net>
2015-05-28 01:14:10 +1000

    removed dependencies

--- manifests/default.pp
+++ manifests/default.pp
@@ -10,6 +10,4 @@ node default {
   Class['java'] -> Class['selenium::hub']
   Class['java'] -> Class['selenium::node']
   Class['display'] -> Class['selenium::node']
-  Class['firefox'] -> Class['selenium::node']
-  Class['chrome'] -> Class['selenium::node']
 }
