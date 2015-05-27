c0910c3f3c565459909ede00501e0ecde9198785
William Tsoi <william@williamtsoi.net>
2015-05-27 08:04:20 +0000

    more changes

--- Puppetfile
+++ Puppetfile
@@ -5,8 +5,8 @@ mod 'puppetlabs/apt'
 mod 'p0deje/display'
 mod 'jhoblitt/selenium'
 mod 'p0deje/firefox'
-mod 'anl/puppet-chrome'
-  :git => 'https://github.com/anl/puppet-chrome.git'
-
+mod 'anl/puppet-chrome',
+  :git => 'https://github.com/anl/puppet-chrome.git',
+  :ref => 'master'
 mod 'rip/module-data',
   :git => 'https://github.com/ripienaar/puppet-module-data.git'
--- manifests/default.pp
+++ manifests/default.pp
@@ -6,9 +6,9 @@ node default {
   class { 'firefox':
     version => '38.0.1-0ubuntu1'
   }
-  include 'google-chrome'
+  include 'chrome'
   Class['java'] -> Class['selenium::hub']
-  Class['java'] -> Class['selenium::node']
-  Class['display'] -> Class['selenium::node']
-  Class['firefox'] -> Class['selenium::node']
+  #Class['java'] -> Class['selenium::node']
+  #Class['display'] -> Class['selenium::node']
+  #Class['firefox'] -> Class['selenium::node']
 }
